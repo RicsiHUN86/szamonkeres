4c780f6e4842f4020f46aebb0c0a7c124353d046
RicsiHUN86 <barnusrichi@gmail.com>
2026-03-18 08:26:46 +0100

    delete

--- deletebooks.cs
+++ deletebooks.cs
@@ -1,10 +1,30 @@
-﻿using System.Linq;
+using System.Linq;
 
 namespace szamonkeres
 {
     internal class Delete
     {
-       
-        
+        LibraryResults libraryResults = new LibraryResults();
+
+        public LibraryResults DeleteBook(int id)
+        {
+            using (var context = new librarydbContext())
+            {
+                var book = context.Books.FirstOrDefault(b => b.Id == id);
+
+                if (book == null)
+                {
+                    libraryResults.Message = "Nincs ilyen könyv";
+                    return libraryResults;
+                }
+
+                context.Books.Remove(book);
+                context.SaveChanges();
+
+                libraryResults.Message = "Sikeres törlés";
+                return libraryResults;
+            }
+        }
+
     }
 }
